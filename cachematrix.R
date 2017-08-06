895c05d98a6bcbd145892754633ea1d3014761e7
Ruchir Vasavada <ruchir.vasavada@gmail.com>
2017-08-06 22:34:15 +0530

    submitting the assignment for the cached inverse matrix script.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script creates a matrix that can cache the inerse of given matrix
 
-## Write a short comment describing this function
+## function to create and initialise getter and setter for main 
+## and inverse matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inversematrix <- NULL
+        
+        ## set the main matrix in a variable.
+        set <- function(x) {
+                x <<- y
+                ## if the matrix is set, then reset the 
+                ## any earlier cache
+                inversematrix <<- NULL
+        }
+        
+        ## return the main matrix
+        get <- function() x 
+        
+        ## set the inverse matrix
+        setinverse <- function(im) inversematrix <<- im
+        
+        ## return the cached inverse matrix
+        getinverse <- function() inversematrix
+        
+        ## returns all the function variable.
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## function to return the cached inverse. It will compute the inverse of 
+## matrix if not found in cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## get inverse of matrix from cache
+        i <- x$getinverse()
+        
+        ## if the value is NULL, then cache does not have the inveser. 
+        ## need to compute the inverse
+        if(is.null(i)) {
+                print("computing matrix inverse...")
+                data <- x$get()
+                
+                ## the inverse is possible only if matrix is a square matrix.
+                ## even if matrix is square the inverse will not be computed
+                ## if matrix is singular. 
+                if(nrow(data) == ncol(data)) {
+                        i <- solve(data)
+                        x$setinverse(i)
+                }
+                else {
+                        #matrix inverse not possible, becuase this is not a
+                        #square matrix.
+                        print("ERROR : inverse is not possible for the given matrix")
+                }
+        }
+        
+        ## retutn the inverse
+        return (i)
 }
